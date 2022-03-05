3e7390779e1fd32cda2c7b64e8b5a7b963374f7e
joshsia <56477506+joshsia@users.noreply.github.com>
2022-03-04 21:32:29 -0800

    Change scale projection of world map

--- src/plot.py
+++ src/plot.py
@@ -163,7 +163,7 @@ def plot_map(xcon):
     
     chart = (
         alt.Chart(datamap).mark_geoshape(stroke='gray')
-        .project(type="mercator", scale=110, translate=[280, 350])
+        .project(type="mercator", scale=90, translate=[280, 350])
         .encode(
             color=alt.Color(
                 field="Salary_USD",
